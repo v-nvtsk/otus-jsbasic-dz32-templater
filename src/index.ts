0bafc80dd66fb62764db61b1009449e1e02a1e7e
v-nvtsk <v@nvtsk.ru>
2024-03-01 00:01:33 +0300

    refactor: remove geoIp call from entry point

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import "./style.css";
 import { Weather } from "./components/weather/weather";
-import getCityByIP from "./api/ip";
+// import getCityByIP from "./api/ip";
 import appendParentWithChild from "./utils/appendParentWithChild";
 
 // Запуск с начальным состоянием
 const app = appendParentWithChild(document.body, "div", "app");
-getCityByIP().then((city) => new Weather(app, { currentCity: city }).init());
+new Weather(app, { currentCity: "tula" }).init();
 
 // // Запуск с пустым начальным состоянием
 // const app2 = appendParentWithChild(document.body, "div", "app");
